76815b46e98a51b490e2f45e2aa5fa2336ef4460
AtmirKetbiev <55296157+AtmirKetbiev@users.noreply.github.com>
2020-04-06 03:11:57 +0300

    implementation MyExecutor

---
+++ src/main/java/task3/MyExecutor.java
@@ -0,0 +1,56 @@
+package task3;
+
+import java.util.Comparator;
+import java.util.concurrent.Executor;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class MyExecutor implements Executor {
+
+    private PriorityBlockingQueue<Thread> priorityBlockingQueue = new PriorityBlockingQueue(1, new ThreadComparator());
+    private int poolVolume;
+    private int numberOfThreads;
+    private Object o;
+
+    public MyExecutor(int poolVolume, int numberOfThreads) {
+        this.poolVolume = poolVolume;
+        this.numberOfThreads = numberOfThreads;
+        o = new Object();
+    }
+
+    @Override
+    public void execute(Runnable runnable) {
+        Thread thread = new Thread(() -> {
+            runnable.run();
+            /*synchronized(o) {
+                o.notify();
+            }*/
+        });
+
+        priorityBlockingQueue.add(thread);
+        Run();
+    }
+
+    private void Run() {
+        if (priorityBlockingQueue.size() < poolVolume && priorityBlockingQueue.size() < numberOfThreads) {
+            return;
+        }
+        for (int i = 0; i < poolVolume; i++) {
+            try {
+                Thread t = priorityBlockingQueue.poll(1, TimeUnit.SECONDS);
+                t.run();
+                System.out.println(t.getName());
+                //o.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class ThreadComparator implements Comparator<Thread> {
+    @Override
+    public int compare(Thread t1, Thread t2) {
+        return 0;
+    }
+}
---
+++ src/test/java/task3/MyExecutorTest.java
@@ -0,0 +1,69 @@
+package task3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class MyExecutorTest {
+
+    @Test
+    public void oneThread() throws IOException, ClassNotFoundException {
+
+        int poolVolume = 1;
+        int numberOfThreads = 1;
+
+        MyExecutor myExecutor = new MyExecutor(poolVolume, numberOfThreads);
+        CountDownLatch countDownLatch = new CountDownLatch(numberOfThreads);
+        myExecutor.execute(countDownLatch::countDown);
+        try {
+            countDownLatch.await(200, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(0, countDownLatch.getCount());
+    }
+
+    @Test
+    public void nThreads() throws IOException, ClassNotFoundException {
+
+        int poolVolume = 3;
+        int numberOfThreads = 3;
+
+        MyExecutor myExecutor = new MyExecutor(poolVolume, numberOfThreads);
+        CountDownLatch countDownLatch = new CountDownLatch(numberOfThreads);
+
+        for(int i = 0; i < numberOfThreads; i++) {
+            myExecutor.execute(countDownLatch::countDown);
+            try {
+                countDownLatch.await(200, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Assert.assertEquals(0, countDownLatch.getCount());
+    }
+
+    @Test
+    public void moreThreads() throws IOException, ClassNotFoundException {
+
+        int poolVolume = 5;
+        int numberOfThreads = 2 * poolVolume + 1;
+
+        MyExecutor myExecutor = new MyExecutor(poolVolume, numberOfThreads);
+        CountDownLatch countDownLatch = new CountDownLatch(numberOfThreads);
+
+        for(int i = 0; i < numberOfThreads; i++) {
+            myExecutor.execute(countDownLatch::countDown);
+            try {
+                countDownLatch.await(100, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Assert.assertEquals(0, countDownLatch.getCount());
+    }
+
+}
